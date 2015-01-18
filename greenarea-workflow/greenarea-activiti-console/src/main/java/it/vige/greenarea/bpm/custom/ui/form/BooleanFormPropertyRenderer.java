d4d6b2a800e4063d87ed8e1b55571cc429809b41
Luca Stancapiano <luca.stancapiano@eng.it>
2015-01-18 18:57:56 +0100

    licenses

--- greenarea-workflow/greenarea-activiti-console/src/main/java/it/vige/greenarea/bpm/custom/ui/form/BooleanFormPropertyRenderer.java
+++ greenarea-workflow/greenarea-activiti-console/src/main/java/it/vige/greenarea/bpm/custom/ui/form/BooleanFormPropertyRenderer.java
@@ -1,16 +1,16 @@
-/* Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
+/******************************************************************************
+ * Vige, Home of Professional Open Source Copyright 2010, Vige, and           *
+ * individual contributors by the @authors tag. See the copyright.txt in the  *
+ * distribution for a full listing of individual contributors.                *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may    *
+ * not use this file except in compliance with the License. You may obtain    *
+ * a copy of the License at http://www.apache.org/licenses/LICENSE-2.0        *
+ * Unless required by applicable law or agreed to in writing, software        *
+ * distributed under the License is distributed on an "AS IS" BASIS,          *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
+ * See the License for the specific language governing permissions and        *
+ * limitations under the License.                                             *
+ ******************************************************************************/
 package it.vige.greenarea.bpm.custom.ui.form;
 
 import java.lang.reflect.Method;
@@ -21,9 +21,6 @@ import org.activiti.engine.impl.form.BooleanFormType;
 import com.vaadin.ui.CheckBox;
 import com.vaadin.ui.Field;
 
-/**
- * @author Frederik Heremans
- */
 public class BooleanFormPropertyRenderer<T> extends
 		GreenareaAbstractFormPropertyRenderer<T> {
 
--- greenarea-workflow/greenarea-activiti-console/src/main/java/it/vige/greenarea/bpm/custom/ui/form/DateFormPropertyRenderer.java
+++ greenarea-workflow/greenarea-activiti-console/src/main/java/it/vige/greenarea/bpm/custom/ui/form/DateFormPropertyRenderer.java
@@ -1,16 +1,16 @@
-/* Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
+/******************************************************************************
+ * Vige, Home of Professional Open Source Copyright 2010, Vige, and           *
+ * individual contributors by the @authors tag. See the copyright.txt in the  *
+ * distribution for a full listing of individual contributors.                *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may    *
+ * not use this file except in compliance with the License. You may obtain    *
+ * a copy of the License at http://www.apache.org/licenses/LICENSE-2.0        *
+ * Unless required by applicable law or agreed to in writing, software        *
+ * distributed under the License is distributed on an "AS IS" BASIS,          *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
+ * See the License for the specific language governing permissions and        *
+ * limitations under the License.                                             *
+ ******************************************************************************/
 package it.vige.greenarea.bpm.custom.ui.form;
 
 import static it.vige.greenarea.Conversioni.addDays;
@@ -28,9 +28,6 @@ import org.vaadin.addons.rangedatefield.RangeDateField;
 import com.vaadin.ui.DateField;
 import com.vaadin.ui.Field;
 
-/**
- * @author Frederik Heremans
- */
 public class DateFormPropertyRenderer<T> extends
 		GreenareaAbstractFormPropertyRenderer<T> {
 
